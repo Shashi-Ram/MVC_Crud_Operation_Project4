44c1eeb16402bc2788ebf3d1383e04c1f1dd0558
Shashi Ram <bharramshashi@outlook.com>
2022-02-21 04:15:43 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿select * from Locations;
+select * from Customers;
+insert into Locations values
+('Noida', 'is the best Place'),
+('Moradabad', 'is the best Place'),
+('Kolkata', 'is the best Place'),
+('Delhi', 'is the best Place'),
+('Ballia ','is the best Place'),
+('Meerut ','is the best Place');
+insert into Customers Values 
+('shashi ram','shashi@gmail.com','94343243',1),
+('Rahul kumar','shashi@gmail.com','94343243',1),
+('deepak umar','shashi@gmail.com','94343243',4),
+('Rajesh kumar','shashi@gmail.com','94343243',5),
+('Ankit kumar','shashi@gmail.com','94343243',3),
+('Shubham kumar','shashi@gmail.com','94343243',2);
---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_Database_Project4.Controllers;
+using MVC_Database_Project4.Models;
+
+namespace MVC_Database_Project4.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> emps = dbContext.Employees.ToList();
+            return View(emps);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            dbContext.Employees.Add(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
